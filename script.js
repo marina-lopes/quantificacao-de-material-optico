7b5c32e5ba4b37601662fc60e77671550bfe8da0
Marina Lopes da Cruz Araújo <104329292+marina-lopes@users.noreply.github.com>
2024-09-16 11:18:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-let numPavimentosEl = parseInt(document.getElementById('num_pavimentos').value);
-let pontosCFTVEl = document.getElementById('pontos_cftv').value === 'sim' ? Array.from(document.querySelectorAll('.cftvPavimento')).reduce((acc, input) => acc + parseInt(input.value), 0) : 0;
-let pontosVoIPEl = document.getElementById('pontos_voip').value === 'sim' ? Array.from(document.querySelectorAll('.voipPavimento')).reduce((acc, input) => acc + parseInt(input.value), 0) : 0;
-let medidaBasicaEl = parseInt(document.getElementById('medida_basica').value);
-let especificacaoCaboEl = document.getElementById('especificacao_cabo').value;
-let velocidadeEl = parseInt(document.getElementById('velocidade').value);
+let numPavimentosEl = document.getElementById('num_pavimentos');
+let pontosCFTVEl = document.getElementById('pontos_cftv');
+let pontosVoIPEl = document.getElementById('pontos_voip');
+let medidaBasicaEl = document.getElementById('medida_basica');
+let especificacaoCaboEl = document.getElementById('especificacao_cabo');
+let velocidadeEl = document.getElementById('velocidade');
 let backboneSecundarioEl = document.getElementById('backbone_secundario');
-let distanciaPrimarioEl = parseInt(document.getElementById('distancia_backbone_primario').value);
+let distanciaPrimarioEl = document.getElementById('distancia_backbone_primario');
 let resultadoEl = document.getElementById('resultado');
 
 const peDireito = 5;
 
 numPavimentosEl.addEventListener('change', function () {
+    const numPavimentos = parseInt(this.value);
     let container = document.getElementById('pavimentosInputContainer');
     container.innerHTML = '';
 
-    for (let i = 1; i <= numPavimentosEl; i++) {
+    for (let i = 1; i <= numPavimentos; i++) {
         let input = document.createElement('div');
         input.innerHTML = `
                     <label for="pavimento_${i}">Número de pontos de telecom no pavimento ${i}: </label>
@@ -30,10 +31,11 @@ pontosCFTVEl.addEventListener('change', function () {
     container.innerHTML = '';
 
     if (possuiCFTV === 'sim') {
-        for (let i = 1; i <= numPavimentosEl; i++) {
+        const numPavimentos = parseInt(numPavimentosEl.value);
+        for (let i = 1; i <= numPavimentos; i++) {
             const input = document.createElement('div');
             input.innerHTML = `
-                <label for="cftv_${i}">Quantidade de câmeras CFTV no pavimento ${i}:</label>
+                <label for="cftv_${i}">Número de câmeras CFTV no pavimento ${i}:</label>
                 <input type="number" id="cftv_${i}" class="cftv_pavimento" required><br>
                 `;
             container.appendChild(input);
@@ -50,11 +52,11 @@ pontosVoIPEl.addEventListener('change', function () {
     container.innerHTML = '';
 
     if (possuiVoIP === 'sim') {
-        const numPavimentos = parseInt(document.getElementById('numPavimentos').value);
+        const numPavimentos = parseInt(numPavimentosEl.value);
         for (let i = 1; i <= numPavimentos; i++) {
             const input = document.createElement('div');
             input.innerHTML =
-                `<label for="voip_${i}">Quantidade de pontos VOIP no pavimento ${i}:</label>
+                `<label for="voip_${i}">Número de pontos VoIP no pavimento ${i}:</label>
                 <input type="number" id="voip_${i}" class="voip_pavimento" required><br>`;
             container.appendChild(input);
         }
@@ -77,42 +79,100 @@ backboneSecundarioEl.addEventListener('change', function () {
     }
 });
 
-function calcularComprimentoFibra(numPavimentosEl) {
+function calcularComprimentoFibra(numPavimentos) {
     comprimentoTotal = 0;
 
-    for (let i = 2; i <= numPavimentosEl; i++) {
+    for (let i = 2; i <= numPavimentos; i++) {
         comprimentoTotal += (i + 1) * peDireito;
     }
 
-    return comprimentoTotal;
+    return comprimentoTotal * 1.2;
 }
 
-/*
-function determinarTipoFibra(velocidadeEl, distanciaPrimarioEl) {
-    let limitesFibra = {
-        om1: { fastEthernet: 2000, gigabit: 275, dezGigabit: 33 },
-        om2: { fastEthernet: 2000, gigabit: 550, dezGigabit: 82 },
-        om3: { fastEthernet: 2000, gigabit: 1000, dezGigabit: 300 },
-        om4: { fastEthernet: 2000, gigabit: 1100, dezGigabit: 400 },
-        om5: { fastEthernet: 2000, gigabit: 1100, dezGigabit: 400 }
+function calcularNumeroFibrasDisponiveis(pontosTelecom, pontosCFTV, pontosVoIP) {
+    let totalFibras = 0;
+    let pontosRede = pontosTelecom * 2;
+
+    // Considerando 1 par reserva para cada disciplina
+    if (pontosCFTV > 0) {
+        totalFibras += 4;
+    }
+    if (pontosVoIP > 0) {
+        totalFibras += 4;
+    }
+    if ((pontosRede - pontosCFTV - pontosVoIP) > 0) {
+        totalFibras += 4;
+    }
+}
+
+function definirTipoFibra(velocidade, distanciaPrimario) {
+    if (velocidade === 1000) {
+        if (distanciaPrimario <= 260) {
+            return {
+                tipo: "Fibra Multimodo 62.5-µm",
+                janela: "850 nm (1000Base-SX)"
+            };
+        } else if (distanciaPrimario <= 550) {
+            return {
+                tipo: "Fibra Multimodo 50-µm",
+                janela: "850 nm (1000Base-SX)"
+            };
+        } else if (distanciaPrimario <= 440) {
+            return {
+                tipo: "Fibra Multimodo 62.5-µm",
+                janela: "1310 nm (1000Base-LX)"
+            };
+        } else if (distanciaPrimario <= 750) {
+            return {
+                tipo: "Fibra Multimodo 50-µm",
+                janela: "1310 nm (1000Base-LX)"
+            };
+        } else if (distanciaPrimario <= 3000) {
+            return {
+                tipo: "Fibra Monomodo 9-µm",
+                janela: "1310 nm (1000Base-LX)"
+            };
+        }
+    } else if (velocidade === 10000) {
+        if (distanciaPrimario <= 300) {
+            return {
+                tipo: "Fibra Multimodo",
+                janela: "850 nm (10GBase-SR)"
+            };
+        } else if (distanciaPrimario <= 10000) {
+            return {
+                tipo: "Fibra Monomodo",
+                janela: "1310 nm (10GBase-LR)"
+            };
+        } else if (distanciaPrimario <= 40000) {
+            return {
+                tipo: "Fibra Monomodo",
+                janela: "1550 nm (10GBase-X ou 10GBase-ER)"
+            };
+        }
+    }
+
+    return {
+        tipo: "Não suportado.",
+        janela: "N/A"
     };
 }
-*/
 
 function calcularInfraestrutura(pontosTelecom, pontosCFTV, pontosVoIP) {
-    let totalPontosRede = (pontosTelecom - pontosVoIP - pontosCFTV) * 2;
-    
-    let qtdeCaixasCabo = Math.ceil((medidaBasicaEl * totalPontosRede)/305);
+    let pontosRede = pontosTelecom * 2;
+
+    let qtdeCaixasCabo = Math.ceil((medidaBasicaEl * pontosRede) / 305);
 
     let numEspelhos = pontosTelecom;
 
-    let numRJ45Femea = pontosTelecom * 2;
+    let numRJ45Femea = pontosRede;
 
-    let patchCordAzul = pontosTelecom * 2 - pontosVoIP - pontosCFTV; // Dados
+    let patchCordAzul = pontosTelecom * 2 - pontosVoIP - pontosCFTV; 
+    let patchCableAzul = pontosRede - pontosCFTV - pontosVoIP; // Dados
     let patchCableVermelho = pontosCFTV; // CFTV
     let patchCableAmarelo = pontosVoIP; // VoIP
 
-    let numPatchPanels = Math.ceil(totalPontosRede / 12);
+    let numPatchPanels = Math.ceil(pontosRede / 12);
     let numSwitches = numPatchPanels;
     let numOrgFrontais = numPatchPanels + numSwitches;
 
@@ -121,7 +181,7 @@ function calcularInfraestrutura(pontosTelecom, pontosCFTV, pontosVoIP) {
 
     let etiquetasPortasPP = numPatchPanels * 12;
     let etiquetasPP = numPatchPanels;
-    let etiquetasPatchCable = patchCableAmarelo + patchCableVermelho;
+    let etiquetasPatchCable = patchCableAzul + patchCableAmarelo + patchCableVermelho;
     let etiquetasTomadasEspelhos = numRJ45Femea + numEspelhos;
     let etiquetasCabosUTP = numRJ45Femea * 2;
 
@@ -136,6 +196,7 @@ function calcularInfraestrutura(pontosTelecom, pontosCFTV, pontosVoIP) {
         numEspelhos,
         numRJ45Femea,
         patchCordAzul,
+        patchCableAzul,
         patchCableVermelho,
         patchCableAmarelo,
         numPatchPanels,
@@ -152,23 +213,30 @@ function calcularInfraestrutura(pontosTelecom, pontosCFTV, pontosVoIP) {
     };
 }
 
-function calcularBackboneOptico(numPavimentosEl) {
-    let comprimentoFibra = calcularComprimentoFibra(numPavimentosEl);
+function calcularBackboneOptico(numPavimentos, pontosTelecom, pontosCFTV, pontosVoIP) {
+    const velocidade = parseInt(velocidadeEl.value);
+    const distanciaPrimario = parseInt(distanciaPrimarioEl.value);
 
-// Calcular
+    let comprimentoFibra = calcularComprimentoFibra(numPavimentos);
+    let numFibras = calcularNumeroFibrasDisponiveis(pontosTelecom, pontosCFTV, pontosVoIP);
 
-    let numDIO;
-    let numAcopladorMM;
-    let numAcopladorSM;
-    let numBandejasDIO;
-    let numTO;
-    let numPigtailMMSimples;
-    let numPigtailMMDuplo;
-    let numCordaoOpticoMM;
-    let numPigtailSMSimples;
-    let numCordaoOpticoSM;
+    let tipoFibraExterna = definirTipoFibra(velocidade, distanciaPrimario);
+    let tipoFibra = definirTipoFibra(velocidade, calcularComprimentoFibra(numPavimentos));
+
+    let numDIO = Math.ceil(numPavimentos * numFibras / 24);
+    let numAcopladorMM = 0;
+    let numAcopladorSM = 0;
+    let numBandejasDIO = 0;
+    let numTO = 0;
+    let numPigtailMMSimples = 0;
+    let numPigtailMMDuplo = 0;
+    let numCordaoOpticoMM = 0;
+    let numPigtailSMSimples = 0;
+    let numCordaoOpticoSM = 0;
 
     return {
+        tipoFibra,
+        tipoFibraExterna,
         comprimentoFibra,
         numDIO,
         numAcopladorMM,
@@ -183,18 +251,26 @@ function calcularBackboneOptico(numPavimentosEl) {
     };
 }
 
-let infraestrutura = calcularInfraestrutura(pontosTelecom, pontosCFTV, voip);
-let backboneOptico = calcularBackboneOptico(numPavimentosEl);
-
 document.getElementById('form').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const numPavimentos = parseInt(numPavimentosEl.value);
+    const pontosTelecom = Array.from(document.querySelectorAll('.pontos_telecom')).reduce((acc, input) => acc + parseInt(input.value || 0), 0);
+    const pontosCFTV = pontosCFTVEl.value === 'sim' ? Array.from(document.querySelectorAll('.cftv_pavimento')).reduce((acc, input) => acc + parseInt(input.value), 0) : 0;
+    const pontosVoIP = pontosVoIPEl.value === 'sim' ? Array.from(document.querySelectorAll('.voip_pavimento')).reduce((acc, input) => acc + parseInt(input.value), 0) : 0;
+
+    let infraestrutura = calcularInfraestrutura(pontosTelecom, pontosCFTV, pontosVoIP);
+    let backboneOptico = calcularBackboneOptico(numPavimentos, pontosTelecom, pontosCFTV, pontosVoIP);
+
     resultadoEl.innerHTML = `
         <h3>Materiais da Infraestrutura da Rede</h3>
-        <p>Quantidade de cabo UTP cat. 6 (MH) (cxs):${infraestrutura.qtdeCaixasCabo} </p
+        <p>Quantidade de cabo UTP cat. 6 (MH) (cxs):${infraestrutura.qtdeCaixasCabo}</p
         <p>Espelhos 4x4 - 2 furações: ${infraestrutura.numEspelhos}</p>
         <p>Tomada RJ45 fêmea cat. 6: ${infraestrutura.numRJ45Femea}</p>
         <p>Patch Cord cat. 6, azul, 3m: ${infraestrutura.patchCordAzul}</p>
+        <p>Patch Cable cat. 6, Dados (azul) - 2,5m: ${infraestrutura.patchCableAzul}</p>
         <p>Patch Cable cat. 6, CFTV (vermelho) - 2,5m: ${infraestrutura.patchCableVermelho}</p>
-        <p>Patch Cable cat. 6, VOIP (amarelo) - 2,5m: ${infraestrutura.patchCableAmarelo}</p>
+        <p>Patch Cable cat. 6, VoIP (amarelo) - 2,5m: ${infraestrutura.patchCableAmarelo}</p>
         <p>Patch Panel cat. 6, 12 portas (PPMH): ${infraestrutura.numPatchPanels}</p>
         <p>Switch: ${infraestrutura.numSwitches}</p>
         <p>Organizador de cabo frontal (1U): ${infraestrutura.numOrgFrontais}</p>
@@ -209,14 +285,16 @@ document.getElementById('form').addEventListener('submit', function (event) {
         <p>Etiquetas de identificação de Cabos UTP - MH: ${infraestrutura.etiquetasCabosUTP}</p>
 
         <h3>Materiais do Backbone Óptico</h3>
-        <p>Tipo de fibra óptica: ${tipoFibra}</p>
+        <p>Tipo de fibra óptica: ${backboneOptico.tipoFibra}</p>
+        <p>Tipo de fibra óptica externa: ${backboneOptico.tipoFibraExterna}</p>
+        <p>Especificação do cabo: ${especificacaoCaboEl}</p>
         <p>Comprimento da fibra óptica: ${backboneOptico.comprimentoFibra} metros</p>
         <p>Chassi DIO com 24 portas - 1U - 19": ${backboneOptico.numDIO}</p>
         <p>Acoplador óptico 50x125µm - MM - LC - duplo: ${backboneOptico.numAcopladorMM}</p>
         <p>Acoplador óptico 9x125µm - SM - LC - duplo: ${backboneOptico.numAcopladorSM}</p>
         <p>Bandeja para emenda de fibra no DIO (12 emendas): ${backboneOptico.numBandejasDIO}</p>
         <p>Terminador Óptico para 8 fibras: ${backboneOptico.numTO}</p>
-        
+
         <p>Pigtail 50x125µm - MM - 1,5m - LC - simples: ${backboneOptico.numPigtailMMSimples}</p>
         <p>Pigtail 50x125µm - MM - 1,5m - LC - duplo: ${backboneOptico.numPigtailMMDuplo}</p>
         <p>Cordão Óptico 50x125µm - MM - 3m - LC - duplo: ${backboneOptico.numCordaoOpticoMM}</p>
